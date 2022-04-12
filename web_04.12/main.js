01a27d4409900c88a55294beadf703e01e1e3274
garam222 <101619265+garam222@users.noreply.github.com>
2022-04-12 15:00:04 +0900

    Add files via upload

---
+++ web_04.12/main.js
@@ -0,0 +1,21 @@
+const vertical = document.querySelector('.vertical');
+const horizontal = document.querySelector('.horizontal');
+const target = document.querySelector('.target');
+const tag = document.querySelector('.tag');
+
+addEventListener('load',function () {
+    const targetRect = target.getBoundingClientRect();
+    const targetHalfwidth = targetRect.width / 2;
+    const targetHalfheight = targetRect.height / 2;
+
+    document.addEventListener('mousemove', (e) => {
+        const x = e.clientX;
+        const y = e.clientY;
+        // console.log (x,y)
+        vertical.style.transform = `translateX(${x}px)`;
+        horizontal.style.transform = `translateY(${y}px)`;
+        target.style.transform = `translate(${x-targetHalfwidth}px, ${y-targetHalfheight}px)`;
+        tag.style.transform = `translate(${x}px, ${y}px)`;
+        tag.innerHTML = `${x}px ${y}px`;
+    })
+})
